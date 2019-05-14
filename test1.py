a46b80140fc6bc7c77ccc804b90561d867ba7a83
sarfaraz <sarfaraz.bittech@gmail.com>
2019-05-14 13:09:53 +0530

    change made

--- test1.py
+++ test1.py
@@ -1,19 +1,78 @@
 
 import os
-import random  # <------ This is for generating random password
+import shutil
+import mysql.connector
+import random# <------ This is for generating random password
+
 
 # for generating random password below three line is important
 s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 passlen = 12
-p =  "".join(random.sample(s,passlen ))
+#p =  "".join(random.sample(s,passlen ))
 # print(f"Your password is {p}")
+
+############################################################################################
+project_name = input("Enter your project name :- ")
+
+
 
 
 project_pass = "".join(random.sample(s,passlen ))
 mysql_pass = "".join(random.sample(s,passlen ))
 
-print(f"Your project password is {project_pass}")
-print(f"Your mysql password is {mysql_pass}")
+print(f"Your project name is :-  {project_name}")
+print(f"Your project password is :- {project_pass}")
+print(f"Your mysql password is :- {mysql_pass}")
 
 
-print(project_pass)
+print("Creating a Directory")
+os.mkdir('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name)
+os.mkdir('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/dev')
+os.mkdir('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/qa')
+os.chmod('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/dev',0o777)
+os.chmod('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/qa',0o777)
+
+if os.path.exists('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name):
+    print("Directory created successfully")
+    os.listdir('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name)
+else:
+    print("Failed to create Directory")
+
+print('Copying the Gitlab')
+shutil.copytree("/Users/sarfaraz/Downloads/gitlab", '/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/gitlab')
+
+if os.path.exists('/Users/sarfaraz/PycharmProjects/pythontesting/'+project_name+'/gitlab'):
+    print("Successfully copied the gitlab file")
+else:
+    print("Failed to copy")
+
+print("Editing the gitlab file")
+s = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php").read()
+s = s.replace('bannershop', project_name)
+f = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php", 'w')
+f.write(s)
+f.close()
+
+s = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php").read()
+s = s.replace('fohC0oobao3i', project_pass)
+f = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php", 'w')
+f.write(s)
+f.close()
+
+print(os.listdir('/Users/sarfaraz/PycharmProjects/pythontesting/testing'))
+
+######################################
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE "+project_name+'_dev')
+mycursor.execute("CREATE DATABASE "+project_name+'_qa')
+
+
+
---
+++ test2.py
@@ -0,0 +1,51 @@
+# s = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php").read()
+# s = s.replace('trustquid', 'testing')
+# f = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php", 'w')
+# f.write(s)
+# f.close()
+#
+# s = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php").read()
+# s = s.replace('fohC0oobao3i', 'testing')
+# f = open("/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab/deploy.php", 'w')
+# f.write(s)
+# f.close()
+
+import shutil
+import os
+import mysql.connector
+
+test = 'testing1'
+testpass = 'test123'
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE "+test+'_dev')
+mycursor.execute("CREATE DATABASE "+test+'_qa')
+mycursor.execute("CREATE USER "+test+'@localhost IDENTIFIED BY '+"'"+testpass+"';")
+mycursor.execute("GRANT ALL PRIVILEGES ON "+test+'_dev'".* "+'TO '+test+"@localhost;")
+mycursor.execute("GRANT ALL PRIVILEGES ON "+test+'_qa'".* "+'TO '+test+"@localhost;")
+mycursor.execute("FLUSH PRIVILEGES;")
+
+mycursor.execute("SHOW DATABASES")
+
+
+for x in mycursor:
+  print(x)
+
+
+# shutil.copytree('/Users/sarfaraz/Downloads/gitlab', '/Users/sarfaraz/PycharmProjects/pythontesting/testing/gitlab')
+# # shutil.copy('/Users/sarfaraz/Downloads/gitlab', '/Users/sarfaraz/PycharmProjects/pythontesting/testing')
+# print(os.listdir('/Users/sarfaraz/PycharmProjects/pythontesting/testing'))
+# test = 'testing'
+# print("CREATE DATABASE "+test+'_dev;')
+#
+# print("CREATE USER "+test+'@localhost IDENTIFIED BY '+"'"+testpass+"';")
+#
+# print("GRANT ALL PRIVILEGES ON "+test+'_dev'".* "+'TO '+test+"@localhost;")
+# print("FLUSH PRIVILEGES;")
